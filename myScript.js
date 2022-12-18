277fd02f7000b5a63319509aea499e3bd5ea9a21
Nicholas Healing <nhealing1@gmail.com>
2022-12-18 22:10:19 +0000

    Add function for single round (input rock) which is case insensitive

--- myScript.js
+++ myScript.js
@@ -6,11 +6,24 @@ let myArray = [
 
 let randomChoice = myArray[Math.floor(Math.random()*myArray.length)];
 
-playerSelection = prompt("Do you choose Rock, Paper or Scissors?");
+playerSelection = prompt("Do you choose Rock, Paper or Scissors?").toLowerCase();
 computerSelection = randomChoice;
 
-console.log(computerSelection);
-
-function rockPaperScissors(playerSelection, computerSelection) {
-
+function rockPaperScissors() {
+    if ((playerSelection == 'rock') && (computerSelection == 'Scissors')) {
+    return 'You win! Rock beats Scissors';
+}   
+    else if ((playerSelection == 'rock') && (computerSelection == 'Paper')) {
+    return 'You lose! Paper beats Rock';
+}   
+    else if ((playerSelection == 'rock') && (computerSelection == 'Rock')) {
+    return 'Rock vs Rock is a draw!';
+    }
+    else {
+        return "You haven't entered a valid choice";
+    }
 }
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(rockPaperScissors(playerSelection, computerSelection));

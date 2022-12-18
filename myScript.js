f58c11a89c49e905887167786c394983274f8ed7
Nicholas Healing <nhealing1@gmail.com>
2022-12-18 22:28:10 +0000

    Edit, adding more options in if statement

--- myScript.js
+++ myScript.js
@@ -9,7 +9,7 @@ let randomChoice = myArray[Math.floor(Math.random()*myArray.length)];
 playerSelection = prompt("Do you choose Rock, Paper or Scissors?").toLowerCase();
 computerSelection = randomChoice;
 
-function rockPaperScissors() {
+function rockPaperScissors(playerSelection, computerSelection) {
     if ((playerSelection == 'rock') && (computerSelection == 'Scissors')) {
     return 'You win! Rock beats Scissors';
 }   
@@ -18,6 +18,25 @@ function rockPaperScissors() {
 }   
     else if ((playerSelection == 'rock') && (computerSelection == 'Rock')) {
     return 'Rock vs Rock is a draw!';
+    }
+    else if ((playerSelection == 'paper') && (computerSelection == 'Scissors')) {
+        return 'You lose! Scissors beats paper';
+    }
+    else if ((playerSelection == 'paper') && (computerSelection == 'Paper')) {
+        return 'Paper vs Paper is a draw!'
+    }
+    else if ((playerSelection == 'paper') && (computerSelection == 'Rock')) {
+        return 'You win! Paper beats Rock'
+    }
+    else if ((playerSelection == 'scissors') && (computerSelection
+        == 'Scissors')) {
+        return 'Scissors vs scissors is a draw!'
+        }
+    else if ((playerSelection == 'scissors') && (computerSelection == 'Paper')) {
+        return 'You win! Scissors beats Paper';
+    }
+    else if ((playerSelection == 'scissors') && (computerSelection == 'Rock')) {
+        return 'You lose! Rock beats Paper'
     }
     else {
         return "You haven't entered a valid choice";
